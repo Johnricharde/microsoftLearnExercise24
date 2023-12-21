456be9d9be7c29873882912fa644aab76e3b6ee7
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-21 16:08:11 +0100

    Add project files.

---
+++ microsoftLearnExercise24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace microsoftLearnExercise24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            double usd = 23.73;
+            int vnd = UsdToVnd(usd);
+
+            Console.WriteLine($"$ {usd} USD = ₫ {vnd} VND");
+
+            Console.WriteLine($"₫ {vnd} VND = $ {VndToUsd(vnd)} USD");
+
+            int UsdToVnd(double usd)
+            {
+                int rate = 23500;
+                return (int)(rate * usd);
+            }
+            double VndToUsd(int vnd)
+            {
+                double rate = 23500;
+                return vnd / rate;
+            }
+        }
+    }
+}
